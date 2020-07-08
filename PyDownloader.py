ce306ea2d1d35dc534c11347b889525f3ac4fc2b
SoskaVLG <57997351+SoskaVLG@users.noreply.github.com>
2020-07-08 17:23:20 +0400

    Add files via upload

---
+++ PyDownloader.py
@@ -0,0 +1,30 @@
+from pytube import YouTube
+import easygui as g
+import os
+hello = g.msgbox("        PyDownloader поможет Вам скачать видео с  YouTube в нужном качестве"                  )
+post = g.msgbox("После скачивания появится окно с указанием пути , по которому хранится скачанный файл")
+url = g.enterbox("  Вставьте ссылку на видео (сочетание ctrl + v на английской раскладке )            ")
+qt1 = g.buttonbox("                         Выберите качество            " , choices= ['  360p  ' , '  480p  ' , '  720p  ' , '  1080p  '])
+if qt1 == "  360p  " :
+     qt = 18
+
+
+elif qt1 == "  480p  " :
+    qt = 13
+
+
+elif qt1 == "  720p  " :
+    qt = 136
+
+
+elif qt1 == "  1080p  " :
+    qt = 137
+
+
+
+
+
+YouTube(str(url)).streams.get_by_itag(int(qt)).download()
+cwd = os.getcwd()
+info = g.msgbox ("Ваше видео находится по пути : " + "\n" + str(cwd))
+
