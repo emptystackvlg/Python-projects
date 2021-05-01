64e18cbe604ef251a55b7dd238d2f5fd226ff9e5
SoskaVLG <krykov976@gmail.com>
2021-05-01 13:33:11 +0300

    Update pc_bot.py

--- pc_bot.py
+++ pc_bot.py
@@ -8,7 +8,8 @@ bot = telebot.TeleBot('1217728629:AAFVe-1pLS4IFZHmuQgJVI19MxTNu1K9vOA')
 
 
 keyboard = telebot.types.ReplyKeyboardMarkup(True)
-keyboard.row ("Браузер","Spotify")
+keyboard.row ("Браузер","Spotify","VS Code")
+keyboard.row ("GitHub Desktop","Visual Studio")
 
 @bot.message_handler (commands=['start'])
 
@@ -19,14 +20,21 @@ def start (message) :
 
 def main (message) : 
     if message.text.lower () == "браузер" : 
-        
         bot.send_message (message.chat.id ,"Запускаю браузер",reply_markup=keyboard)
-       
         ToastNotifier().show_toast("BOT","Запускаю Google Chrome",threaded=True)
         os.system ('"C:\Program Files\Google\Chrome\Application\chrome.exe"')
+    
     elif message.text.lower () == "spotify" :
         ToastNotifier().show_toast("BOT","Запускаю Spotify" , threaded=True)
         os.system('"C:/Users/Alexandr/AppData/Roaming/Spotify/Spotify.exe"')
+
+    elif message.text.lower() == "vs code" : 
+        ToastNotifier().show_toast("BOT","Запускаю Visual Studio Code" , threaded=True)
+        os.system( '"C:/Users/Alexandr/AppData/Local/Programs/Microsoft VS Code/Code.exe"' )   
+
+    elif message.text.lower()=="github desktop" : 
+        ToastNotifier().show_toast("BOT" , "Запускаю GitHub Desktop" , threaded=True)
+        os.system('C:/Users/Alexandr/AppData/Local/GitHubDesktop/GitHubDesktop.exe')
 
       
 
