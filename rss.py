a38f977e50157cbd1e3531954acb5d8b3e282e3c
SoskaVLG <krykov976@gmail.com>
2021-05-07 17:39:29 +0300

    Update rss.py
    Добавил новости с 4PDA

--- rss.py
+++ rss.py
@@ -5,8 +5,11 @@ print("Бот работает")
 bot = telebot.TeleBot('1763819681:AAHp6CEje8vyPNti_uMEQ2mNb7vOevX8Ui4')
 
 keyboard = telebot.types.ReplyKeyboardMarkup(True)
-keyboard.row = "ЧИТАТЬ НОВОСТИ"
+keyboard.row ("ЧИТАТЬ НОВОСТИ")
 
+switch = telebot.types.ReplyKeyboardMarkup(True)
+switch.row ('ДАЛЕЕ------>>>')
+switch.row ('<<<------НАЗАД В ГЛАВНОЕ МЕНЮ')
 
 @bot.message_handler(commands=['start'])
 def start(message):
@@ -15,11 +18,12 @@ def start(message):
 
 @bot.message_handler(content_types=['text'])
 def main(message):
+    
     if message.text.lower() == "читать новости":
        NewsFeed = feedparser.parse("https://4pda.ru/feed/")
        for i in range(0,(len(NewsFeed.entries))):
             entry = NewsFeed.entries[i]
-            bot.send_message(message.chat.id, entry.link,reply_markup = keyboard)
+            bot.send_message(message.chat.id, entry.link,reply_markup = switch)
             i += 1
 
   
@@ -28,5 +32,3 @@ def main(message):
 
 
 bot.polling(none_stop=True)
-
-
