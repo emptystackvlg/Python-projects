581a96be1cc38fea179a6b2f0cc5b6988267b84a
SoskaVLG <krykov976@gmail.com>
2021-07-20 22:23:44 +0300

    Update class_train.py
    Add main logic

--- class_train.py
+++ class_train.py
@@ -1,3 +1,7 @@
+import os 
+import time 
+os.system("cls")
+
 class Hot_Dog () :
     def __init__ (self , meat , addons , souse):
 
@@ -6,17 +10,33 @@ class Hot_Dog () :
         self.souse = souse 
 
     def cook (self) : 
-        print ("Ваша сосиска готовится") 
+        print ("Ваша сосиска готовится" + "\n") 
+        time.sleep(5)
 
-    def pay (self) : 
-        print ("Вам нужно заплатить , прежде чем получить сосиску")
+    def pay (self) :
+        final_price = int(0)
+        if self.meat == "курица" :
+            final_price += 50
+        elif self.meat == "свинина" :
+            final_price += 70 
+        elif self.meat == "говядина" :
+            final_price += 70
+        if self.addons == "сыр" :
+            final_price += 30
+        elif self.addons == "капуста" or self.addons == "лук" :
+            final_price += 15
+        final_price +=30 
+     
+        print ("Вам нужно заплатить " + str(final_price)+" У.Е. , прежде чем получить сосиску" + "\n")
+        print ("Введите нужную сумму и подтвердите операцию , введя 'ОПЛАТИТЬ' :" + "\n")
+        check_price = int (input("> "))        
+        сonfirm = str (input("> "))   
+        print ("Операция  успешно выполнена ! " + "\a\n")
 
     def take (self):
-
-        print ("Получите распишитесь :" + "\n" + "сосиска из " + self.meat + " c " + self.addons + " и " + self.souse)
+        print ("Получите и распишитесь :" + "\n" + "сосиска из " + self.meat + " c " + self.addons + " и " + self.souse + "\n")
 
     def bay (self) : 
-
         print ("Спасибо за покупку , всего хорошего !!!!")
 
 My_Hot_Dog = Hot_Dog("курица" , "сыр" , "горчица")
@@ -25,4 +45,4 @@ My_Hot_Dog.pay()
 My_Hot_Dog.cook()
 My_Hot_Dog.take()
 My_Hot_Dog.bay()
-    
+
