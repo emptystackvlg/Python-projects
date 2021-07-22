6f003b90bc331c528a04bd8d134301f48a109a01
SoskaVLG <krykov976@gmail.com>
2021-07-22 19:02:49 +0300

    Update class_train.py

--- class_train.py
+++ class_train.py
@@ -3,84 +3,85 @@ import time
 
 os.system("cls")
 
-class Hot_Dog () :
-    def __init__ (self , meat , addons , souse):
+
+class Hot_Dog ():
+    def __init__(self, meat, addons, souse):
 
         self.meat = meat
-        self.addons  = addons
-        self.souse = souse 
+        self.addons = addons
+        self.souse = souse
 
-    def cook (self) : 
-        print ("Ваша сосиска готовится" + "\n") 
-        i = int (0)
-        for i in range (1,6) :
-            print ("." * i)
+    def cook(self):
+        print("Ваша сосиска готовится" + "\n")
+        i = int(0)
+        for i in range(1, 6):
+            print("." * i)
             time.sleep(1)
-        os.system ("cls")
+        os.system("cls")
 
-    def pay (self) :
+    def pay(self):
         final_price = int(0)
-        if self.meat == "курица" :
+        if self.meat == "курица":
             final_price += 50
-        elif self.meat == "свинина" :
-            final_price += 70 
-        elif self.meat == "говядина" :
+        elif self.meat == "свинина":
             final_price += 70
-        if self.addons == "сыр" :
+        elif self.meat == "говядина":
+            final_price += 70
+        if self.addons == "сыр":
             final_price += 30
-        elif self.addons == "капуста" or self.addons == "лук" :
+        elif self.addons == "капуста" or self.addons == "лук":
             final_price += 15
-        final_price +=30 
-     
-        print ("Вам нужно заплатить " + str(final_price)+" У.Е. , прежде чем получить сосиску" + "\n")
-        print ("Введите нужную сумму и подтвердите операцию , введя 'ОПЛАТИТЬ' :" + "\n")
-        check_price = int (input("> ")) 
-        сonfirm = str (input("> "))   
-        print ("Операция  успешно выполнена ! " + "\n")
+        final_price += 30
 
-    def take (self):
-        print ("Получите и распишитесь :" + "\n" + "сосиска из " + self.meat + " c " + self.addons + " и " + self.souse + "\n")
+        print("Вам нужно заплатить " + str(final_price) +
+              " У.Е. , прежде чем получить сосиску" + "\n")
+        print("Введите нужную сумму и подтвердите операцию , введя 'ОПЛАТИТЬ' :" + "\n")
+        check_price = int(input("> "))
+        сonfirm = str(input("> "))
+        print("Операция  успешно выполнена ! " + "\n")
 
-    def bay (self) : 
-        print ("Спасибо за покупку , всего хорошего !!!!")
+    def take(self):
+        print("Получите и распишитесь :" + "\n" + "сосиска из " +
+              self.meat + " c " + self.addons + " и " + self.souse + "\n")
 
-
-
-
+    def bay(self):
+        print("Спасибо за покупку , всего хорошего !!!!")
 
 
 os.system("cls")
-print ("Выберите мясо , из которого будет сделана сосиска :  " + "\n" + "1. Курица " + "\n" + "2. Свинина" + "\n" + "3. Говядина " + "\n")
-meat_choice = int (input("> "))
-if meat_choice == 1 :
+print("Выберите мясо , из которого будет сделана сосиска :  " + "\n" +
+      "1. Курица " + "\n" + "2. Свинина" + "\n" + "3. Говядина " + "\n")
+meat_choice = int(input("> "))
+if meat_choice == 1:
     first = "курица"
-elif meat_choice == 2 :
-    first = "свинина" 
-elif meat_choice == 3 :
+elif meat_choice == 2:
+    first = "свинина"
+elif meat_choice == 3:
     first = "говядина"
 os.system("cls")
 
-print ("\n" + "Выберите дополнительный ингредиент : " + "\n" + "1. Сыр " +"\n" + "2. Капуста" + "\n" + "3. Лук" + "\n")
-addons_choice = int (input ("> "))
-if addons_choice == 1 :
+print("\n" + "Выберите дополнительный ингредиент : " + "\n" +
+      "1. Сыр " + "\n" + "2. Капуста" + "\n" + "3. Лук" + "\n")
+addons_choice = int(input("> "))
+if addons_choice == 1:
     second = "сыр"
-elif addons_choice == 2 :
+elif addons_choice == 2:
     second = "капуста"
-elif addons_choice == 3 :
+elif addons_choice == 3:
     second = "лук"
 os.system("cls")
 
-print ("\n" + "Выберите соус : " + "\n" + "1. Кетчуп " + "2. Горчица" + "\n")
-souse_choice = int (input ("> "))
-if souse_choice == 1 :
-    third = "кетчуп"    
-elif souse_choice == 2 :
+print("\n" + "Выберите соус : " + "\n" + "1. Кетчуп " + "2. Горчица" + "\n")
+souse_choice = int(input("> "))
+if souse_choice == 1:
+    third = "кетчуп"
+elif souse_choice == 2:
     third = "горчица"
-os.system ("cls")
+os.system("cls")
 
-My_Hot_Dog = Hot_Dog(first,second,third)
+My_Hot_Dog = Hot_Dog(first, second, third)
 
-My_Hot_Dog.cook ()
+My_Hot_Dog.cook()
 My_Hot_Dog.pay()
 My_Hot_Dog.take()
 My_Hot_Dog.bay()
