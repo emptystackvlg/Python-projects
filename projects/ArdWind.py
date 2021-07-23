96943b7a0c9eee421d77468681248c07823afbeb
SoskaVLG <krykov976@gmail.com>
2021-07-23 15:58:45 +0300

    Delete files

--- projects/ArdWind.py
+++
@@ -1,7 +0,0 @@
-import serial 
-
-ser = serial.Serial ('COM3' ,9600)
-
-current = ser.read ()
-
-print (current)
--- projects/covid_monitor.py
+++
@@ -1,6 +0,0 @@
-from covid import Covid
-
-covid = Covid()
-data = covid.get_status_by_country_id(142)
-
-print (data)
