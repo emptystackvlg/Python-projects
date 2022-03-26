bbae507d1c965dc41031bd8448faf7ec0d5cae18
Krykov Alexandr <57997351+emptystackvlg@users.noreply.github.com>
2022-03-26 12:49:57 +0300

    Update down_cmd_version.py

--- projects/Downloader_qt/Downloader_cmd/down_cmd_version.py
+++ projects/Downloader_qt/Downloader_cmd/down_cmd_version.py
@@ -3,8 +3,6 @@ from os import startfile , chdir, system
 def install_libs () :
         system ("pip install easygui")
         system ("pip install win10toast")
-        system ("pip install PyQt6")
-        system ("pip install PySide6")
         system ("pip install pyperclip")
         system ("pip install yt_dlp")
         system ("cls")
@@ -13,73 +11,35 @@ def install_libs () :
         exit (0)
         
 try :
-    import threading 
-    from PyQt6 import QtCore, QtGui, QtWidgets
-    from win10toast_click import ToastNotifier as tf 
+    from win10toast import ToastNotifier as tf 
     from yt_dlp import YoutubeDL
     from easygui import diropenbox
     from pyperclip import paste
+    from pyfiglet import Figlet
 except: 
     install_libs ()
 
-
-class Ui_Dialog(object):
-    def setupUi(self, Dialog):
-        Dialog.setObjectName("Dialog")
-        Dialog.resize(152, 244)
-        Dialog.setMaximumSize(QtCore.QSize(152,244))
-        Dialog.setMinimumSize(QtCore.QSize(152,244))
-        Dialog.setStyleSheet("background {\n"
-"rgb(223, 238, 237)}")
-        self.download = QtWidgets.QPushButton(Dialog)
-        self.download.setGeometry(QtCore.QRect(10, 140, 131, 101))
-        self.download.setObjectName("download")
-        self.download.clicked.connect(down)
-        self.label = QtWidgets.QLabel(Dialog)
-        self.label.setGeometry(QtCore.QRect(10, 10, 141, 111))
-        self.label.setObjectName("label")
-
-        self.retranslateUi(Dialog)
-        QtCore.QMetaObject.connectSlotsByName(Dialog)
-
-    def retranslateUi(self, Dialog):
-        _translate = QtCore.QCoreApplication.translate
-        Dialog.setWindowTitle(_translate("Dialog", "PyDownloader"))
-        self.download.setText(_translate("Dialog", "СКАЧАТЬ"))
-        self.label.setText(_translate("Dialog", "<html><head/><body><p><span style=\" font-size:11pt; font-weight:700;\">Скопируйте ссылку </span></p><p><span style=\" font-size:11pt; font-weight:700;\">и нажмите кнопку</span></p></body></html>"))
-
-def down():
+def youtube_downloader () :
     try:
-        Dialog.hide()
-        link = paste()
+        text = Figlet (font = 'slant')
+        print (text.renderText("VSTU SHEDULE"))
+        link = input ("Введите ссылку на видео\n>")
         if link == '':
-           Dialog.show()
+            print ("Ссылка пустая")
         save = diropenbox(title="Выберите папку для сохранения")
         ydl_opts = {
-                'quiet': True
-                 }
-        def open () :
-            startfile(save)
+            'quiet': True
+                    }
         with YoutubeDL(ydl_opts) as ydl:
             tf().show_toast("PyDownloader","Скачивание началось",duration=5,threaded=True)
             chdir(save)
             ydl.download([str(link)])
-            
-            tf().show_toast("PyDownloader","Ваш файл скачался , нажмите чтобы открыть",duration=10,threaded=True, 
-            callback_on_click=open)
-            Dialog.show()
+                
+            tf().show_toast("PyDownloader","Ваш файл скачался , нажмите чтобы открыть",duration=10,threaded=True)
+                
     except:
-        tf().show_toast("PyDownloader","Что-то пошло не так,попробуйте еще раз",duration=10,threaded=True,callback_on_click= None)
-        Dialog.show()
-if __name__ == "__main__":
-    import sys
-    app = QtWidgets.QApplication(sys.argv)
-    app.setStyle("Fusion")
-    Dialog = QtWidgets.QDialog()
-    ui = Ui_Dialog()
-    ui.setupUi(Dialog)
-    main_thread = threading.Thread (target= Dialog.show())
-    sys.exit(app.exec())
-
+        tf().show_toast("PyDownloader","Что-то пошло не так,попробуйте еще раз",duration=10,threaded=True)
+        
+youtube_downloader()
 
 
