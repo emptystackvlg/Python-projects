745a86f02936c8c6ea9359f5245e916597a5e651
Krykov Alexandr <57997351+emptystackvlg@users.noreply.github.com>
2022-03-28 12:39:31 +0300

    Update down_cmd_version.py

--- projects/Downloader_qt/Downloader_cmd/down_cmd_version.py
+++ projects/Downloader_qt/Downloader_cmd/down_cmd_version.py
@@ -1,44 +1,34 @@
 from __future__ import unicode_literals
 from os import startfile , chdir, system
-def install_libs () :
-        system ("pip install easygui")
-        system ("pip install win10toast")
-        system ("pip install pyperclip")
-        system ("pip install yt_dlp")
-        system ("cls")
-        print ("Библиотеки установлены, перезапустите программу")
-        system ("pause")
-        exit (0)
-        
-try :
-    from win10toast import ToastNotifier as tf 
-    from yt_dlp import YoutubeDL
-    from easygui import diropenbox
-    from pyperclip import paste
-    from pyfiglet import Figlet
-except: 
-    install_libs ()
+import sys
+from win10toast import ToastNotifier as tf 
+from yt_dlp import YoutubeDL
+from pyfiglet import Figlet
+import easygui as gui
+
 
 def youtube_downloader () :
+    text = Figlet (font = 'slant')
+    print (text.renderText("Py_Downloader"))
+    link = str ()
+    link = input ("Введите ссылку на видео\n> ")
+    if link == '':
+        system ("cls")
+        youtube_downloader()
+    save = gui.diropenbox(title="Выберите папку для сохранения")
     try:
-        text = Figlet (font = 'slant')
-        print (text.renderText("VSTU SHEDULE"))
-        link = input ("Введите ссылку на видео\n>")
-        if link == '':
-            print ("Ссылка пустая")
-        save = diropenbox(title="Выберите папку для сохранения")
-        ydl_opts = {
-            'quiet': True
-                    }
+        ydl_opts = {}
         with YoutubeDL(ydl_opts) as ydl:
-            tf().show_toast("PyDownloader","Скачивание началось",duration=5,threaded=True)
             chdir(save)
             ydl.download([str(link)])
-                
-            tf().show_toast("PyDownloader","Ваш файл скачался , нажмите чтобы открыть",duration=10,threaded=True)
-                
+        print ("Загрузка завершена !")
+        system ("pause")
+        exit ()
     except:
-        tf().show_toast("PyDownloader","Что-то пошло не так,попробуйте еще раз",duration=10,threaded=True)
+        print ("Ошибка,попробуйте еще раз")
+        system ("pause")
+        system ("cls")
+        youtube_downloader()        
         
 youtube_downloader()
 
