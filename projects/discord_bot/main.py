18a60ee91659f9e1d62071843b9c4a4cdea9a4bc
Krykov Alexandr <57997351+emptystackvlg@users.noreply.github.com>
2022-04-16 20:24:34 +0300

    Update

--- projects/discord_bot/main.py
+++ projects/discord_bot/main.py
@@ -18,4 +18,4 @@ async def on_message(message):
     if message.content.lower() == "info":
         await message.channel.send ("Это тестовый Discord бот")
 
-client.run('token')
+client.run('token' )
