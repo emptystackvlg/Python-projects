8e89fe2a6c9754403e38430078ed4953947d9239
Krykov Alexandr <57997351+emptystackvlg@users.noreply.github.com>
2022-05-20 22:36:24 +0300

    Update

--- projects/old projects/mail.py
+++ projects/old projects/mail.py
@@ -1,6 +1,6 @@
 ﻿import smtplib
 mail = smtplib.SMTP('smtp.gmail.com', 587)
 mail.starttls()
-mail.login('krykov976@gmail.com','12345Pravda??')
+mail.login('krykov976@gmail.com','password')
 mail.sendmail("krykov976@gmail.com","krykov976@gmail.com","Привет как дела ?".encode('utf8'))
 mail.close()
