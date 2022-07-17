337bb1ecb506d8a3ebc6a602903b0bcf8c1fa330
Krykov Alexandr <57997351+emptystackvlg@users.noreply.github.com>
2022-07-17 18:30:13 +0300

    Update music_converter.py

--- projects/Downloader_qt/Downloader_cmd/music_converter.py
+++ projects/Downloader_qt/Downloader_cmd/music_converter.py
@@ -1,12 +1,20 @@
 import  os
 from easygui import diropenbox
-from moviepy.editor import * #подключаем пакет moviepy
+from moviepy.editor import * 
 
-directory = diropenbox ("Выбериите папку")
+directory = diropenbox ("Выбериите папку с видео")
 files = os.listdir(directory) 
 print('\n'.join(files)) 
 print (type (files))
+dir_to_save = diropenbox ("Выберите папку для сохранения музыки")
 for file in files: 
     audioclip = AudioFileClip(directory+"\\"+file) 
-    audioclip.write_audiofile(directory+"\\"+file+".mp3") 
+    audioclip.write_audiofile(dir_to_save+"\\"+file+".mp3") 
     print(file+".mp3")
+perm_for_del = str (input ("Удалить видео ? (Y/N)\n> "))
+if perm_for_del == "Y":
+    for file in files : 
+        os.remove(directory + "\\" + file)
+
+else:
+    exit (0)
