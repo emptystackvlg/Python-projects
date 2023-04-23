771c5c8b20779cdffd4fb616540549d8b7adb77e
Krykov Alexandr <57997351+emptystackvlg@users.noreply.github.com>
2023-04-23 15:50:32 +0300

    Float

--- teor_ver/Laba_6.py
+++ teor_ver/Laba_6.py
@@ -2,7 +2,7 @@ mass = []
 with open('C:\\Users\\Alexandr\\Documents\\GitHub\\Python-projects\\teor_ver\\file.txt',"r") as file:
     lines = file.readlines()
     for line in lines: 
-        read_line = [int(x) for x in line.split()]
+        read_line = [float(x) for x in line.split()]
         mass.append(read_line)
     for i in range (len(mass)):
         print ("i = " + str(i))
