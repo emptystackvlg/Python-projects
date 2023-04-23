42f32ae0475ce26b0dba5faf769c65e570adee28
Krykov Alexandr <57997351+emptystackvlg@users.noreply.github.com>
2023-04-23 15:48:32 +0300

    Сделал построковое чтение из txt файла

---
+++ teor_ver/Laba_6.py
@@ -0,0 +1,10 @@
+mass = []
+with open('C:\\Users\\Alexandr\\Documents\\GitHub\\Python-projects\\teor_ver\\file.txt',"r") as file:
+    lines = file.readlines()
+    for line in lines: 
+        read_line = [int(x) for x in line.split()]
+        mass.append(read_line)
+    for i in range (len(mass)):
+        print ("i = " + str(i))
+        for j in mass[i] :
+            print (j)
