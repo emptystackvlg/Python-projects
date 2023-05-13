66aefae4368e3146472fe94848ba7b36452a66be
Krykov Alexandr <57997351+emptystackvlg@users.noreply.github.com>
2023-05-13 13:31:39 +0300

    Сделал все расчеты

--- Laba_5/2.py
+++ Laba_5/2.py
@@ -28,29 +28,63 @@ def make_intervals (main_mass):
     print ("Шаг равен : " + str (h))
     for i in range (num_of_intervals):
         current_x = float (x_min)
-        """ if i == 0:
-            intervals[i] = [float (current_x - dh/2),float ((current_x - dh/2) + h)]
-        elif i == num_of_intervals:
-            intervals[i] = [float (current_x+h*i),float (current_x + dh/2 + h*(i+1))]
-        else: """
         intervals [i] = [float (current_x + h*i),float (current_x + h*(i+1))]
-            
     for key in intervals.keys():
         print (intervals[key])
-
     return intervals
-def intervals_sort (main_mass,intervals):
-    sort_intervals = []
+
+def intervals_freq (main_mass,intervals):
+    freq_intervals = []
     for key in intervals.keys():
         ni = 0 
         for var in main_mass:
             if (intervals[key][0]<= var <intervals[key][1]):
                 ni += 1
-        sort_intervals.append (ni) 
-    if sum(sort_intervals) == len (main_mass):
-        return sort_intervals            
+        freq_intervals.append (ni) 
+    if (sum (freq_intervals) == len (main_mass)):
+        return freq_intervals            
+    else :
+        print ("Ошибка, сумма ni != nss")
+        exit(0)
+def relative_freq (mass_of_ni, count_n):
+    rel_freq = []
+    for ni in mass_of_ni:
+        ni /= count_n
+        rel_freq.append (ni)
+    return rel_freq
+
+def mid_of_intervals (intervals):
+    mid = []
+    for key in intervals.keys():
+        ci = sum(intervals[key]) / 2
+        mid.append (ci)
+    return mid
+
+def params (mass_of_ni,mid):
+    x_v = 1 
+    summ = 0
+    for i in range (len(mass_of_ni)):
+        summ += mass_of_ni [i] * mid[i]
+    x_v = float("{0:.3f}".format(summ/len(mass_of_ni)))
+    summ = 0
+    for i in range (len(mass_of_ni)):
+        summ += mass_of_ni[i] * ((mid[i] - x_v)**2)
+    d_v = float("{0:.3f}".format(summ/len (mass_of_ni)))
+    Sigma_v = float("{0:.3f}".format(sqrt (d_v)))
+    parametrs = {'x_v': x_v, 'd_v' : d_v, 'Sigma_v' : Sigma_v}
+    return parametrs
+
+
 main_mass = input_var()
 main_mass.sort()
 print (main_mass)
 intervals = make_intervals(main_mass)
-print (intervals_sort(main_mass,intervals))
+print ("Частоты :" + str(intervals_freq(main_mass,intervals)))
+mass_ni = intervals_freq(main_mass,intervals)
+rel_freq = relative_freq (mass_ni,len (main_mass))
+print ("Относительные частоты : " + str (rel_freq))
+mid = mid_of_intervals (intervals)
+parametrs = params (mass_ni,mid)
+print ("Параметры равны : \n")
+for key in parametrs.keys():
+    print (key + " : " + str(parametrs[key]))
