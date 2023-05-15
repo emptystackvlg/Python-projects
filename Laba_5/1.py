ef5fc68599b093432dbef0d5377989cb68a92bec
Krykov Alexandr <57997351+emptystackvlg@users.noreply.github.com>
2023-05-15 19:52:57 +0300

    Доделал первый номер

--- Laba_5/1.py
+++ Laba_5/1.py
@@ -1,7 +1,6 @@
-from colorama import Fore,Back,Style
 from easygui import fileopenbox
 import matplotlib.pyplot as plt
-from os import system
+from os import system,path
 from math import sqrt
 def input_var ():
     system("cls")
@@ -43,41 +42,44 @@ def F_x (sort_mass,rel_freq):
             summ = 0 
             for j in range (i):
                 summ += rel_freq[j]
-            variables.append(float("{0:.3f}".format(summ)))
+            variables.append(summ)
         else : 
             variables.append(0)
-    variables.append(float("{0:.1f}".format((sum(rel_freq)))))
+    variables.append(sum(rel_freq))
     strings = []
+    variables_to_show = []
+    for var in variables:
+        var = float("{0:.4f}".format(var))
+        variables_to_show.append(var)
     for i in range (len(sort_mass)):
-        if (i!=0):
-            strings.append ("F_x = " + str(variables[i]) + "\t при \t\t" + str(sort_mass[i-1]) + " < " + " x  <=  " + str (sort_mass[i]))
+        if (i == 0):
+            strings.append ("F*(x) = " + str (variables_to_show[i]) + " \t при\t\t" + "x  <=  " + str (sort_mass[i]))
         else : 
-            print ("\n\n")
-            strings.append ("F_x = " + str (variables[i]) + " \t при\t\t" + "x  <=  " + str (sort_mass[i]))
-    strings.append("F_x = " + str (variables[len(sort_mass)]) + "\t при \t\t" + "x  >  " + str(sort_mass[len(sort_mass)-1]))
+            strings.append ("F*(x) = " + str(variables_to_show[i]) + "\t при \t\t" + str(sort_mass[i-1]) + " < " + " x  <=  " + str (sort_mass[i]))
+            
+    strings.append("F*(x) = " + str (variables_to_show[len(sort_mass)]) + "\t при \t\t" + "x  >  " + str(sort_mass[len(sort_mass)-1]))
     for i in strings:
         print (i + "\n\n")
-    return variables
+    return variables_to_show
 
 
 def params (mass,freq):
     n = len (mass)
     summ = 0
-    X_v = (1/n) * sum(mass)
+    X_v = float("{0:.4f}".format((1/n) * sum(mass)))
     for i in range (len(freq)):
         summ += ((freq[i] - X_v)**2*mass[i])
-    D_v = (1/n) * summ
-    Sigma_v = sqrt(D_v)
+    D_v = float("{0:.4f}".format((1/n) * summ))
+    Sigma_v = float("{0:.4f}".format(sqrt(D_v)))
     return (X_v,D_v,Sigma_v)
 
 def make_plot_fx (sorted_mass,variables):
     plt.grid()
     plt.xlim (0,max(sorted_mass)+1)
-    plt.ylim (0,1.2)
+    plt.ylim (0,1.05)
     plt.xlabel (r'$x$')
-    plt.ylabel(r'$F_x$')
+    plt.ylabel(r'$F*(x)$')
     plt.title ("График эмпирической функции распределения")
-    legend_x = []
     x_mass = []
     y_mass = []
     for x in sorted_mass:
@@ -95,6 +97,8 @@ def make_plot_fx (sorted_mass,variables):
             for i in range(2):
                 y_mass.append(y)
     plt.plot (x_mass,y_mass)
+    plt.savefig('f_x_plot.png')
+    print ("\nГрафик сохранён в директории " + str(path.abspath('f_x_plot.png')))
     plt.show ()
 
 def make_plot_of_freq (mass_x,mass_y):
@@ -104,57 +108,105 @@ def make_plot_of_freq (mass_x,mass_y):
     plt.xlabel (r'$xi$')
     plt.ylabel(r'$ni$')
     plt.title ("Полигон частот")
-    legend_x = []
-    legend_y = []
-    for i in range (len(mass_x)):
-        x = mass_x[i]
-        y = mass_y[i]
-        x = float("{0:.4f}".format(x))
-        y = float("{0:.4f}".format(y))
-        legend_x.append(x)
-        legend_y.append(y)
     plt.plot (mass_x,mass_y, '-ro',markersize = 5)
+    plt.savefig('freq_pol.png')
+    print ("\nПолигон сохранён в директории " + str(path.abspath('freq_pol.png')))
     plt.show()  
 
 def make_plot_of_rel_freq (mass_x,mass_y):
     plt.grid()
     plt.xlim (0,max(mass_x)+1)
-    plt.ylim (0,1)
+    plt.ylim (0,max(mass_y) + 0.05)
     plt.xlabel (r'$xi$')
-    plt.ylabel(r'$ni/n$')
+    plt.ylabel(r'$ni/n (wi)$')
     plt.title ("Полигон относительных частот")
-    legend_x = []
-    legend_y = []
-    for i in range (len(mass_x)):
-            x = mass_x[i]
-            y = mass_y[i]
-            x = float("{0:.4f}".format(x))
-            y = float("{0:.4f}".format(y))
-            legend_x.append(x)
-            legend_y.append(y)
     plt.plot (mass_x,mass_y, '-ro',markersize = 5)
+    plt.savefig('rel_freq_pol.png')
+    print ("\nПолигон сохранён в директории " + str(path.abspath('rel_freq_pol.png')))
     plt.show()  
 
 
-def show_table (main_mass,sorted_mass,mass_freq):
-    print ("Вариационный ряд: \n" + str (main_mass))
-    print (Fore.GREEN + "|x|",end = "")
-    for i in sorted_mass:
-        print (Fore.RED + "|" + str(i) + "|",end="")
-    print (Style.RESET_ALL)
-    print (Fore.LIGHTYELLOW_EX + "|n|",end = "")
-    for i in mass_freq:
-        print (Fore.BLUE + "| " + str(i) + " |",end="")
+    
 
 mass = input_var()
 mass.sort()
 
-vars = (F_x(sort_mass(mass),relative_freq(sum(freq(mass)),freq(mass))))
-sorted_mass = sort_mass(mass)
-#make_plot_of_freq (sorted_mass,freq(mass))
-#make_plot_of_rel_freq (sorted_mass,relative_freq(len(mass),freq(mass)))
-make_plot_fx(sorted_mass,vars)
 
-#print (params (sort_mass(mass),freq(mass)))
 
-show_table (mass,sort_mass(mass),freq(mass))
+def menu ():
+    system("cls")
+    print ("Выберите нужный пункт : \n")
+    print ("\t1. Составление вариационного ряда и вывод его на экран\n\n"+"\t2. Составление и вывод на экран статистического ряда частот и построение полигона\n\n" + 
+           "\t3. Составление и вывод на экран статистического ряда относительных частот и построение полигона\n\n"+"\t4. Нахождение эмпирической функции распределения F*(x) вывод ее на экран и построение графика\n\n"
+           +"\t5. Вычисление числовых характеристик и вывод на экран полученных результатов\n\n"+"\tВведите 0 для выхода\n\n")
+    mode = int(input("> "))
+    sorted_mass = sort_mass(mass)
+    freq_mass = freq (mass)
+    rel_freq_mass = relative_freq(len(mass),freq_mass)
+
+
+    if (mode == 0):
+        system("cls")
+        exit(0)
+    elif (mode == 1):
+        system ("cls")
+        print ("Вариационный ряд: \n\n" + str (mass))
+        print ("\n")
+        system("pause")
+        menu()
+    elif (mode == 2):
+        system("cls")
+        print ("Статистический ряд частот: \n")
+        print ("x  :  " + str (sorted_mass))
+        print ("-------" * len(sorted_mass))
+        print ("ni :  " + str (freq_mass))
+        make_plot_of_freq (sorted_mass,freq_mass)
+        print ("\n")
+        system ("pause")
+        menu ()
+    elif (mode == 3):
+        system("cls")
+        print ("Статистический ряд относительных частот: \n")
+        print ("x  :  " + str (sorted_mass))
+        print ("---------" * len(sorted_mass))
+        rel_freq_to_show = []
+        for i in rel_freq_mass:
+            i = float("{0:.4f}".format(i))
+            rel_freq_to_show.append(i)
+        print ("wi :  " + str (rel_freq_to_show))
+        make_plot_of_rel_freq(sorted_mass,rel_freq_mass)
+        print ("\n")
+        system ("pause")
+        menu ()
+    elif (mode == 4):
+        system("cls")
+        print ("Эмпирическая функция распределения F*(x) :\n")
+        variables = F_x(sorted_mass,rel_freq_mass)
+        make_plot_fx(sorted_mass,variables)
+        print ("\n")
+        system ("pause")
+        menu ()
+    elif (mode == 5):
+        system ("cls")
+        print ("Числовые характеристики : \n")
+        parameters = params(mass,freq_mass)
+        print ("\tX_v = " + str(parameters[0]) + "\n")
+        print ("\tD_v = " + str(parameters[1]) + "\n")
+        print ("\tSigma_v = " + str(parameters[2]) + "\n")
+        print ("\n")
+        system ("pause")
+        menu ()
+menu()
+
+
+
+
+
+
+
+
+
+
+
+
+
